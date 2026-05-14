5911491fa175ff032e4b239738ae82852932ea74
stephanom7 <smartinez6@byupathway.edu>
2026-05-13 23:45:46 -0500

    Ejercicio 1 completado

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Pedir el primer nombre
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Pedir el apellido
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Mostrar el resultado en el formato solicitado
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
