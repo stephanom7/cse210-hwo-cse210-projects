426ea01e4381fb9c598405d66fc0e5578322dcac
stephanom7 <smartinez6@byupathway.edu>
2026-05-14 00:06:42 -0500

    Ejercicio 3 completado

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generar número aleatorio entre 1 y 100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1; // valor inicial para entrar al bucle
+
+        // Bucle hasta que el usuario adivine
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
