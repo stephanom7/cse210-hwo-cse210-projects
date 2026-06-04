8b2d670ca77acef42415da131a99fd06e8d3c9e7
stephanom7 <smartinez6@byupathway.edu>
2026-06-04 13:28:26 -0500

    Implement Week05 inheritance activity: Assignment, MathAssignment, WritingAssignment classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,15 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Probar MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        // Probar WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
