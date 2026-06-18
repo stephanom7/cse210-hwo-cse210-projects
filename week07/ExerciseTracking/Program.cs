b0fad43e01e176bb08a4b99a75d6029593d08a64
stephanom7 <smartinez6@byupathway.edu>
2026-06-17 21:39:37 -0500

    Final Activity

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("03 Nov 2022", 40, 20.0),
+            new Swimming("03 Nov 2022", 25, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
